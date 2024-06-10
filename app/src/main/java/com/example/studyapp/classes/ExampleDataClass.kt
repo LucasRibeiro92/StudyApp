b5665966811015fb869263061b1ea3bff4f01dcb
LucasRibeiro92 <lucasc.ribeiroo@gmail.com>
2024-06-09 22:58:42 -0300

    DataClass study.

---
+++ app/src/main/java/com/example/studyapp/classes/ExampleDataClass.kt
@@ -0,0 +1,8 @@
+package com.example.studyapp.classes
+
+data class ExampleDataClass(
+    var numeroInteiro: Int,
+    var numeroDecimal: Double,
+    var numFlutuante: Float,
+    var exampleBoolean: Boolean
+)
